1c722f045f10348feecfd6552c7648eb956e6c3c
Jordan Griveau <jgriveau@yahoo.fr>
2022-03-30 20:07:54 +0200

    /client/src/

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import { Container, Divider } from "@chakra-ui/react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import './App.css';
+import NavMenu from "./components/NavMenu";
+import Contacts from "./components/Contacts";
+
+
+function App() {
+  return (
+    <Container>
+      <Container textAlign="right" m="10px">
+        <NavMenu m="10px" />
+        <Divider />
+      </Container>
+
+      <Routes>
+        <Route path="/" element={<PlaceHolderComponent />} />
+        <Route path="/tunnel" element={<PlaceHolderComponent />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/contacts/:id" element={<PlaceHolderComponent />} />
+      </Routes>
+
+    </Container>
+  );
+}
+
+export default App;
+
+
+const PlaceHolderComponent = () => {
+  const location = useLocation();
+  return (
+    <div>
+      <h1>PlaceHolderComponent, current url: {location.pathname}</h1>
+    </div>
+  );
+}
+
