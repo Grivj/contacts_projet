35316278454956b2aca31b9cca5f829320895848
Jordan Griveau <jgriveau@yahoo.fr>
2022-04-01 11:18:20 +0200

    Hide the form if there are no more contacts
    If there are no more contacts to call, hide the form and instead, display a message.

--- client/src/components/Tunnel.jsx
+++ client/src/components/Tunnel.jsx
@@ -7,13 +7,12 @@ import {
   SwitchControl,
 } from "formik-chakra-ui";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import * as Yup from "yup";
 
 export const Tunnel = () => {
   const [contact, setContact] = useState({});
   const toast = useToast();
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`/api/tunnel`)
@@ -29,61 +28,73 @@ export const Tunnel = () => {
 
   return (
     <Box>
-      <Text fontSize="2em">{contact.name}</Text>
-      <Text fontSize="4em">{contact.phone_number}</Text>
+      {console.log(contact.length)}
+      {console.log(contact)}
+      {Object.keys(contact).length === 0 ? (
+        <Text fontSize="1.5em" textAlign="center">
+          No more contact to call... <br />
+          <Link to="/contacts" style={{ textDecoration: "underline" }}>
+            Go add some!
+          </Link>
+        </Text>
+      ) : (
+        <>
+          <Text fontSize="2em">{contact.name}</Text>
+          <Text fontSize="4em">{contact.phone_number}</Text>
+          <Formik
+            initialValues={contact}
+            validateOnMount
+            onSubmit={async (values, actions) => {
+              const response = await fetch(`/api/tunnel/${contact.id}`, {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+              }).then();
 
-      <Formik
-        initialValues={contact}
-        validateOnMount
-        onSubmit={async (values, actions) => {
-          const response = await fetch(`/api/tunnel/${contact.id}`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(values),
-          }).then();
+              if (response.ok) {
+                actions.setSubmitting(false);
+                toast({
+                  title: "Contact updated.",
+                  description: "Reloading in 2s.",
+                  status: "success",
+                  duration: 2000,
+                  isClosable: true,
+                });
+                setTimeout(() => window.location.reload(), 2000);
+              }
+            }}
+            validationSchema={Yup.object({
+              siren: Yup.string()
+                .matches(/^[0-9]+$/, "Must be only digits")
+                .min(14, "Must be exactly 14 digits")
+                .max(14, "Must be exactly 14 digits")
+                .nullable(),
+              called: Yup.boolean().equals(
+                [true],
+                "Must be checked before submitting."
+              ),
+            })}
+            enableReinitialize
+          >
+            {(formik, props) => (
+              <Form>
+                <InputControl name="siren" label="SIREN number" />
+                <br />
+                <SwitchControl name="called" label="Called" />
 
-          if (response.ok) {
-            actions.setSubmitting(false);
-            toast({
-              title: "Contact updated.",
-              description: "Reloading in 2s.",
-              status: "success",
-              duration: 2000,
-              isClosable: true,
-            });
-            setTimeout(() => window.location.reload(), 2000);
-          }
-        }}
-        validationSchema={Yup.object({
-          siren: Yup.string()
-            .matches(/^[0-9]+$/, "Must be only digits")
-            .min(14, "Must be exactly 14 digits")
-            .max(14, "Must be exactly 14 digits")
-            .nullable(),
-          called: Yup.boolean().equals(
-            [true],
-            "Must be checked before submitting."
-          ),
-        })}
-        enableReinitialize
-      >
-        {(formik, props) => (
-          <Form>
-            <InputControl name="siren" label="SIREN number" />
-            <br />
-            <SwitchControl name="called" label="Called" />
-
-            <ButtonGroup mt="40px">
-              <SubmitButton disabled={!(formik.isValid && formik.isValid)}>
-                Submit
-              </SubmitButton>
-              <ResetButton>Reset</ResetButton>
-            </ButtonGroup>
-          </Form>
-        )}
-      </Formik>
+                <ButtonGroup mt="40px">
+                  <SubmitButton disabled={!(formik.isValid && formik.isValid)}>
+                    Submit
+                  </SubmitButton>
+                  <ResetButton>Reset</ResetButton>
+                </ButtonGroup>
+              </Form>
+            )}
+          </Formik>
+        </>
+      )}
     </Box>
   );
 };
